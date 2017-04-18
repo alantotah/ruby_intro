8ba5fa2fd0563f01f700e9dead3b028a3e3b1800
alantotah <alantotah@users.noreply.github.com>
2017-04-17 22:35:21 -0500

    v1

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,7 +2,20 @@ class ProgramsController < ApplicationController
   def home
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    my_birthday = Time.parse("May 13, 1990")
+    today = Time.now
+    seconds_since_i_was_born = today - my_birthday
+
+    @your_output = seconds_since_i_was_born
+
+    @your_output = seconds_since_i_was_born
+
+    first = "Alan"
+    last = "Totah"
+
+    @first_then_last = first + " " + last
+
+    @last_then_first = last + ", " + first
 
     render("programs/home.html.erb")
   end
@@ -10,7 +23,18 @@ class ProgramsController < ApplicationController
   def first_program
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    rock = 1
+    scissors = 2
+    paper = 3
+    the_number = rand(3)
+
+    if the_number == 1
+      @your_output = "Rock" + "," + " " + "Tie"
+    elsif the_number == 2
+      @your_output = "Scissors" + "," + " " + "Lost"
+    else
+      @your_output = "Paper" + "," + " " + "Won"
+    end
 
     render("programs/first_program.html.erb")
   end
@@ -18,8 +42,16 @@ class ProgramsController < ApplicationController
   def second_program
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    our_numbers = [4, 10, 6]        # Create an array of numbers
+    squared_numbers = []            # Create an empty array
 
+    our_numbers.each do |num|       # For each element in numbers, (refer to it as "num")
+      square = num * num            # Square the number
+      squared_numbers.push(square)  # Push it into the squared_numbers array
+    end
+
+    @your_output = squared_numbers.sum  # Sum the squares
+    
     render("programs/second_program.html.erb")
   end
 
